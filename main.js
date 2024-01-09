e181fa190f8607ef3d625511d6cb3c501482cb26
Bozo2023 <ulrob1.0@email.com>
2024-01-09 08:30:52 +0100

    treolkj

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const factor=0.2;
+const factor=0.21;
+const min=0.2;
+const max=0.4
 let km=document.getElementById("km");
 let age=document.getElementById("eta");
+costo= km*0.21
+document.getElementById("good").addEventListener("click",function(){
+costo.value=(km.value*factor)-(min*(km.value*factor))
+})
